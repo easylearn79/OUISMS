f11189f76802c8991648d3b6e3740f2d59eb8c6a
Musah Abdulazeez <47467648+easylearn79@users.noreply.github.com>
2021-03-14 05:31:37 -0400

    Create settings.py

--- sms/settings.py
+++ sms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ates_d67wquk((7=+$u1qy1_tl^*ypiny@9zhnw3c$6k&b=6pq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ouism.herokuapp.com']
 
